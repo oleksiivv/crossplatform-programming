30fd0a030bcb8105ccde2f0a4c8fde443a5b0731
Alex <oleksii.petrochenko.pz.2019@lpnu.ua>
2021-09-25 17:13:24 +0300

    [JAVA-3] Lab 2 code refactoring

--- Laba-2/src/com/EasyStreet/Main.java
+++ Laba-2/src/com/EasyStreet/Main.java
@@ -33,7 +33,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         Scanner sc=new Scanner(System.in);
 
         LibraryItemsManager manager = new LibraryItemsManager();
